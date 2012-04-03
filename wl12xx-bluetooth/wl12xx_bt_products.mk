cdcbae354e72e5f1e4788f541670009423f3893f
Abhijeet Kumbhar <x0112741@ti.com>
2012-04-03 12:08:28 -0500

    18xx: Copy 18xx BT firmware to system/etc/firmware
    Signed-off-by: Abhijeet Kumbhar <x0112741@ti.com>

--- wl12xx-bluetooth/wl12xx_bt_products.mk
+++ wl12xx-bluetooth/wl12xx_bt_products.mk
@@ -1,6 +1,7 @@
 PRODUCT_COPY_FILES += \
     hardware/ti/wpan/wl12xx-bluetooth/firmware/TIInit_10.6.15.bts:system/etc/firmware/TIInit_10.6.15.bts \
-    hardware/ti/wpan/wl12xx-bluetooth/firmware/TIInit_7.6.15.bts:system/etc/firmware/TIInit_7.6.15.bts
+    hardware/ti/wpan/wl12xx-bluetooth/firmware/TIInit_7.6.15.bts:system/etc/firmware/TIInit_7.6.15.bts \
+    hardware/ti/wpan/wl12xx-bluetooth/firmware/TIInit_12.7.27.bts:system/etc/firmware/TIInit_12.7.27.bts \
 
 ifeq ($(TARGET_PRODUCT), am335xevm)
 PRODUCT_COPY_FILES += \
